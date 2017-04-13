493b8e9195bf5cd369563977b65079298da4f9be
Sam Burrell <sburrell@Sams-MacBook-Air-4.local>
2017-04-13 11:35:31 -0500

    testing

--- Soundboard/ViewController.swift
+++ Soundboard/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var test = 77
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
 }
 
